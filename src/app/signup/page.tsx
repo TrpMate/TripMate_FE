877bdaace26d8b04df08fdc7940683a493b64f42
PureunKang <pureun0000@gmail.com>
2024-11-30 16:57:13 +0900

    chore: 회원가입 페이지 내 import 경로 수정 (FormMessage 삭제)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Input } from '@/components/ui/input'
