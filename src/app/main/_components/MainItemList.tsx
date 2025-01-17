52cf4bee810c4404b49e5a76e1e2d4178e4b4910
Minyoung <alsendrha@naver.com>
2025-01-17 13:09:52 +0900

    api 정리

--- src/app/main/_components/MainItemList.tsx
+++ src/app/main/_components/MainItemList.tsx
@@ -1,82 +1,69 @@
-"use client";
+'use client'
 
-import React, { useEffect, useState } from "react";
-import MenuItem from "./MenuItem";
-import axios from "axios";
-import Skeleton from "./Skeleton";
+import React, { useEffect, useState } from 'react'
+import MenuItem from './MenuItem'
+import Skeleton from './Skeleton'
+import { getData } from '../_api'
 
 type MainItemListProps = {
-  addr1: string;
-  addr2: string;
-  areacode: number;
-  booktour: number;
-  cat1: string;
-  cat2: string;
-  cat3: string;
-  contentid: number;
-  contenttypeid: number;
-  cpyrhtDivCd: string;
-  createdtime: number;
-  firstimage: string;
-  firstimage2: string;
-  mapx: number;
-  mapy: number;
-  mlevel: number;
-  modifiedtime: number;
-  sigungucode: number;
-  tel: string;
-  title: string;
-};
+  addr1: string
+  addr2: string
+  areacode: number
+  booktour: number
+  cat1: string
+  cat2: string
+  cat3: string
+  contentid: number
+  contenttypeid: number
+  cpyrhtDivCd: string
+  createdtime: number
+  firstimage: string
+  firstimage2: string
+  mapx: number
+  mapy: number
+  mlevel: number
+  modifiedtime: number
+  sigungucode: number
+  tel: string
+  title: string
+}
 
 const MainItemList = () => {
-  const [tourList, setTourList] = useState<MainItemListProps[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const getTourList = async () => {
+  const [tourList, setTourList] = useState<MainItemListProps[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getListData = async () => {
     try {
-      setIsLoading(true);
-      const response = await axios.get(
-        `https://tripmate-be.shop/tourAPI/tour-searchKeyword`,
-        {
-          params: {
-            numOfRows: 4,
-            pageNo: 1,
-            keyword: "서울",
-            arrange: "A",
-          },
-        }
-      );
-      if (response.status === 200) {
-        setTourList(response.data.response.body.items.item);
-        console.log(response);
-        setIsLoading(false);
+      setIsLoading(true)
+      const data = await getData({ numOfRows: 4, pageNo: 1, keyword: '서울', arrange: 'A' })
+      if (data.status === 200) {
+        setTourList(data.data.response.body.items.item)
+        setIsLoading(false)
       }
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  };
+    } catch (error) {}
+  }
 
   useEffect(() => {
-    getTourList();
-  }, []);
+    getListData()
+  }, [])
 
   if (isLoading)
     return (
-      <div className="w-full flex justify-center gap-[40px] flex-wrap mt-[111px]">
+      <div className="mt-[111px] flex w-full flex-wrap justify-center gap-[40px]">
         {Array.from({ length: 4 }).map((_, index) => (
           <Skeleton key={index} />
         ))}
         ;
       </div>
-    );
+    )
 
   return (
-    <div className="w-full flex justify-center gap-[40px] flex-wrap mt-[111px]">
+    <div className="mt-[111px] flex w-full flex-wrap justify-center gap-[40px]">
       {tourList.map((item) => (
         <MenuItem key={item.contentid} item={item} />
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default MainItemList;
+export default MainItemList
