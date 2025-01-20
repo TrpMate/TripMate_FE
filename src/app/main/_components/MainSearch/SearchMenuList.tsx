75ae7573b671a11e6238caa3ea5baacf24a2c6fd
Minyoung <alsendrha@naver.com>
2025-01-20 14:01:56 +0900

    메인 페이지 검색, 리스트 수정

--- src/app/main/_components/MainSearch/SearchMenuList.tsx
+++ src/app/main/_components/MainSearch/SearchMenuList.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
-
 type SearchMenuListProps = {
   setCategoryName: (name: string) => void
   setMenuOpen: (open: boolean) => void
+  setListType?: (listType: number) => void
 }
 
-const SearchMenuList = ({ setCategoryName, setMenuOpen }: SearchMenuListProps) => {
-  const categoryList: any = []
+const SearchMenuList = ({ setCategoryName, setMenuOpen, setListType }: SearchMenuListProps) => {
+  const CategoryList = [
+    { name: '관광지', code: 12 },
+    { name: '문화시설', code: 14 },
+    { name: '축제공연행사', code: 15 },
+    { name: '여행코스', code: 25 },
+    { name: '레포츠', code: 28 },
+    { name: '숙박', code: 32 },
+    { name: '쇼핑', code: 38 },
+    { name: '음식점', code: 39 },
+  ]
   return (
     <div className="absolute top-[60px] z-20 flex w-[160px] flex-col items-center rounded-[20px] border bg-white py-3">
-      {categoryList.map((category: any, index: number) => (
+      {CategoryList.map((category) => (
         <div
           className="cursor-pointer py-[6px] font-medium"
-          key={index}
+          key={category.code}
           onClick={() => {
             setCategoryName(category.name)
             setMenuOpen(false)
+            setListType && setListType(category.code)
           }}
         >
           {category.name}
