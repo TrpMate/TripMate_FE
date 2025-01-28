15032ee6766602c27fc3dd741f40ce83de4345ed
Minyoung <alsendrha@naver.com>
2025-01-28 11:33:59 +0900

    플레이스 css 수정

--- src/app/place/_components/placeBanner/PlaceTopBanner.tsx
+++ src/app/place/_components/placeBanner/PlaceTopBanner.tsx
@@ -4,7 +4,7 @@ import PlaceBannerTitle from './PlaceBannerTitle'
 const PlaceTopBanner = () => {
   return (
     <div>
-      <div className="relative flex h-[370px] w-full items-center justify-center overflow-hidden">
+      <div className="relative flex h-[370px] w-full justify-center overflow-hidden">
         <div className="flex flex-col items-center pb-[85px] pt-[100px]">
           <PlaceBannerTitle />
         </div>
