5ce34dbad1428e312b2b9a19075cfd0bd1c78516
Minyoung <alsendrha@naver.com>
2025-01-31 11:23:23 +0900

    플랜리스트 화면 css수정

--- src/app/planList/page.tsx
+++ src/app/planList/page.tsx
@@ -1,19 +1,19 @@
-import PlanCreateButton from "./_components/PlanCreateButton";
-import PlanItemList from "./_components/planitemList/PlanItemList";
-import PlanListMenuButton from "./_components/PlanListMenuButton";
-import PlanListTitle from "./_components/PlanListTitle";
+import PlanCreateButton from './_components/PlanCreateButton'
+import PlanItemList from './_components/planitemList/PlanItemList'
+import PlanListMenuButton from './_components/PlanListMenuButton'
+import PlanListTitle from './_components/PlanListTitle'
 
 const PlanListPage = () => {
   return (
-    <div className="pt-[100px] pb-[171px] flex flex-col items-center justify-center">
-      <div className="w-full pt-[100px] px-[443px]">
+    <div className="flex flex-col items-center justify-center pb-[171px] pt-[100px]">
+      <div className="w-[1000px] pt-[100px]">
         <PlanListTitle />
         <PlanListMenuButton />
         <PlanCreateButton />
         <PlanItemList />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PlanListPage;
+export default PlanListPage
