41efb871c6c4f8fe7f82806eecdc29d6d255497d
Minyoung <alsendrha@naver.com>
2025-01-31 11:33:46 +0900

    fix/플랜리스트 버튼 클릭시 css변경

--- src/app/planList/_components/PlanListMenuButton.tsx
+++ src/app/planList/_components/PlanListMenuButton.tsx
@@ -1,23 +1,35 @@
+'use client'
+
+import { useState } from 'react'
+
 const PlanListMenuButton = () => {
+  const [isClicked, setIsClicked] = useState(0)
   const buttonList = [
-    { id: 1, name: "내 정보" },
-    { id: 2, name: "내 플랜 보기" },
-    { id: 3, name: "문의" },
-  ];
+    { id: 1, name: '내 정보' },
+    { id: 2, name: '내 플랜 보기' },
+    { id: 3, name: '문의' },
+  ]
   return (
-    <div className="pt-[30px] flex justify-center gap-[10px] items-center">
+    <div className="flex items-center justify-center gap-[10px] pt-[30px]">
       {buttonList.map((button) => (
         <div
           key={button.id}
-          className="rounded-[30px] border border-[#CCCCCC] py-[10.5px] px-[19px]"
+          className={`rounded-[30px] border ${
+            isClicked === button.id ? 'border-[#AED400] bg-[#FBFFE6]' : 'border-[#CCCCCC] bg-white'
+          } cursor-pointer px-[19px] py-[10.5px]`}
+          onClick={() => setIsClicked(button.id)}
         >
-          <div className="h-[20px] flex items-center">
-            <p className="leading-none">{button.name}</p>
+          <div className="flex h-[20px] items-center">
+            <p
+              className={`leading-none ${isClicked === button.id ? 'text-[#AED400]' : 'text-black'}`}
+            >
+              {button.name}
+            </p>
           </div>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default PlanListMenuButton;
+export default PlanListMenuButton
